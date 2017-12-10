de32d3bb73f1dd1b9f41cab919e732b92e926fcc
renuzit <renuzit@btcz.life>
2017-12-10 03:59:59 -0600

    Support for Android
    This commit fixes android dependencies so that it can build and release
    an adroid build.

--- src/js/controllers/preferencesBwsUrl.js
+++ src/js/controllers/preferencesBwsUrl.js
@@ -25,15 +25,15 @@ angular.module('copayApp.controllers').controller('preferencesBwsUrlController',
       switch ($scope.bwsurl.value) {
         case 'prod':
         case 'production':
-          bws = 'https://bws.bitcoinz.ph/bws/api'
+          bws = 'https://bws.btcz.rocks/bws/api'
           break;
         case 'sta':
         case 'staging':
-          bws = 'https://bws.bitcoinz.ph/bws/api'
+          bws = 'https://bws.btcz.rocks/bws/api'
           break;
         case 'loc':
         case 'local':
-          bws = 'https://bws.bitcoinz.ph/bws/api'
+          bws = 'https://bws.btcz.rocks/bws/api'
           break;
       };
       if (bws) {

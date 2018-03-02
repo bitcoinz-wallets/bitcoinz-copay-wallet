9b03e5d3c712a1ca06628ae3ad517faa97e7d8c1
Beshoy Girgis <ODSLabs@GMail.com>
2018-03-01 21:17:14 -0600

    Update domain names (#34)
    Updating domain names to migrate us off of bitcoinz.ph since before we
    know it, it'll be that time.

--- src/js/controllers/tx-details.js
+++ src/js/controllers/tx-details.js
@@ -16,7 +16,7 @@ angular.module('copayApp.controllers').controller('txDetailsController', functio
     $scope.isShared = $scope.wallet.credentials.n > 1;
     $scope.txsUnsubscribedForNotifications = config.confirmedTxsNotifications ? !config.confirmedTxsNotifications.enabled : true;
 
-    blockexplorerUrl = 'bitcoinz.ph';
+    blockexplorerUrl = 'explorer.btcz.rocks';
 
     txConfirmNotification.checkIfEnabled(txId, function(res) {
       $scope.txNotification = {
--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -72,7 +72,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
     },
 
     rates: {
-      url: 'https://masq.bitcoinz.ph/rates',
+      url: 'https://masq.btcz.rocks/rates',
     },
 
     release: {
